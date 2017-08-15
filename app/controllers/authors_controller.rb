c455676d811ae8ac46405e155d0344c046664219
SchruteFarms23 <romanmullady@gmail.com>
2017-08-15 09:35:07 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,12 +4,21 @@ class AuthorsController < ApplicationController
   end
 
   def new
-  end
+
+end
 
   def create
-    @author = Author.create!(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.new(author_params)
+    if @author.valid?
+      # If--and only if--the post is valid, do what we usually do.
+      @author.save
+      # This returns a status_code of 302, which instructs the browser to
+      # perform a NEW REQUEST! (AKA: throw @post away and let the show action
+      # worry about re-reading it from the database)
+      redirect_to author_path(@author)
+    else
+      render :new
+  end
   end
 
   private
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,18 @@ class PostsController < ApplicationController
   def update
     @post.update(post_params)
 
-    redirect_to post_path(@post)
+    if @post.valid?
+      # If--and only if--the post is valid, do what we usually do.
+      @post.save
+      # This returns a status_code of 302, which instructs the browser to
+      # perform a NEW REQUEST! (AKA: throw @post away and let the show action
+      # worry about re-reading it from the database)
+      redirect_to post_path(@post)
+    else
+
+    render :edit
   end
+end
 
   private
 
